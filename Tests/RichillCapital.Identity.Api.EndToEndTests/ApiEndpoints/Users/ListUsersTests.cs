e04f6be7aa06e19ae315d6dedffcb4e831cd3f68
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-01-07 10:25:05 +0800

    List Users test

--- Tests/RichillCapital.Identity.Api.EndToEndTests/ApiEndpoints/Users/ListUsersTests.cs
+++ Tests/RichillCapital.Identity.Api.EndToEndTests/ApiEndpoints/Users/ListUsersTests.cs
@@ -1,13 +1,19 @@
+using System.Net;
+using System.Text.Json;
+
 using FluentAssertions;
 
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+using RichillCapital.Identity.Api.Endpoints.Users;
 
 namespace RichillCapital.Identity.Api.EndToEndTests.ApiEndpoints.Users;
 
 [TestClass]
 public sealed class CreateSimulatedAccountTests
 {
+    private const string Route = "/api/users";
+
     private static TestContext _testContext;
     private static WebApplicationFactory<Program> _factory;
 
@@ -19,12 +25,15 @@ public sealed class CreateSimulatedAccountTests
     }
 
     [TestMethod]
-    public async Task Should()
+    public async Task When_RequestIsValid_Should_ReturnSuccess()
     {
         using var client = _factory.CreateClient();
 
-        var response = await client.GetAsync(new Uri("/api/users"));
+        var response = await client.GetAsync(Route);
+        var content = await response.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<IEnumerable<UserResponse>>(content);
 
-        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        result.Should().HaveCount(2);
     }
 }
